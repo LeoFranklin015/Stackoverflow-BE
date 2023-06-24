73a87d0c32bfa2162f2db2909e97f1474f9ccb3a
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2023-06-24 18:06:54 +0530

    get method bug fixes

--- controllers/Questions.js
+++ controllers/Questions.js
@@ -18,7 +18,7 @@ export const AskQuestion = async (req, res) => {
 
 export const getAllQuestions = async (req, res) => {
   try {
-    const questionList = await Question.find();
+    const questionList = await Questions.find();
     res.status(200).json(questionList);
   } catch (error) {
     res.status(404).json({ message: error.message });
