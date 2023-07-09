8dd1dc0e87a47cb9bbbd90240b87d32b55176082
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2023-07-09 11:20:54 +0530

    user Schema changed according to socio

--- models/auth.js
+++ models/auth.js
@@ -1,15 +1,24 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  about: { type: String },
-  tags: { type: [String] },
-  joinedOn: { type: Date, default: Date.now },
-  subscription: { type: String, default: "Free" },
-  lastPostedDate: { type: Date },
-  noOfQuestionsPosted: { type: Number, default: 0 },
-});
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    about: { type: String },
+    tags: { type: [String] },
+    joinedOn: { type: Date, default: Date.now },
+    subscription: { type: String, default: "Free" },
+    lastPostedDate: { type: Date },
+    noOfQuestionsPosted: { type: Number, default: 0 },
+    profilePicture: String,
+    coverPicture: String,
+    followers: [],
+    following: [],
+    worksAt: String,
+    livesIn: String,
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("User", userSchema);
