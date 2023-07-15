548f1821037a208136e19a63b77e389a943a9609
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2023-07-15 12:03:18 +0530

    Login bug fixed.

--- controllers/auth.js
+++ controllers/auth.js
@@ -34,22 +34,26 @@ export const login = async (req, res) => {
   try {
     const existingUser = await users.findOne({ email });
     if (!existingUser) {
-      return res.status(404).json({ message: "No User found ..." });
+      return res.status(404).json({ message: "No User found..." });
     }
 
-    const isPassworcrct = await bcrypt.compare(password, existingUser.password);
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
 
-    if (!isPassworcrct) {
-      return res.status(400).json({ message: "Password Doesnt match !" });
-    } else {
-      const token = jwt.sign(
-        { email: existingUser.email, id: existingUser._id },
-        process.env.JWT_SECRET,
-        { expiresIn: "1h" }
-      );
-      res.status(200).json({ result: existingUser, token });
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ message: "Invalid credentials." });
     }
+
+    const token = jwt.sign(
+      { email: existingUser.email, id: existingUser._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({ result: existingUser, token });
   } catch (error) {
-    res.status(500).json("Something Went Wrong...");
+    res.status(500).json({ message: "Something went wrong..." });
   }
 };
